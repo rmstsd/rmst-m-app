3a992c42602d2a56c31c93781b2c3f1348b0fc1a
rmstsd <rmstsd@qq.com>
2025-07-21 15:29:02 +0800

    init

--- app-example/app/(tabs)/index.tsx
+++ app-example/app/(tabs)/index.tsx
@@ -1,35 +1,30 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Image } from 'expo-image'
+import { Platform, StyleSheet } from 'react-native'
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from '@/components/HelloWave'
+import ParallaxScrollView from '@/components/ParallaxScrollView'
+import { ThemedText } from '@/components/ThemedText'
+import { ThemedView } from '@/components/ThemedView'
 
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
+      headerImage={<Image source={require('@/assets/images/partial-react-logo.png')} style={styles.reactLogo} />}
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome! aaasd</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
+          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes. Press{' '}
           <ThemedText type="defaultSemiBold">
             {Platform.select({
               ios: 'cmd + d',
               android: 'cmd + m',
-              web: 'F12',
+              web: 'F12'
             })}
           </ThemedText>{' '}
           to open developer tools.
@@ -37,9 +32,7 @@ export default function HomeScreen() {
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
+        <ThemedText>{`Tap the Explore tab to learn more about what's included in this starter app.`}</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
@@ -52,24 +45,24 @@ export default function HomeScreen() {
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
+    gap: 8
   },
   stepContainer: {
     gap: 8,
-    marginBottom: 8,
+    marginBottom: 8
   },
   reactLogo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
-  },
-});
+    position: 'absolute'
+  }
+})
---
+++ app/index.tsx
@@ -0,0 +1,113 @@
+import { CameraType, CameraView, useCameraPermissions } from 'expo-camera'
+import { useEffect, useState } from 'react'
+import { AppState, Button, Linking, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+export default function App() {
+  const [facing, setFacing] = useState<CameraType>('back')
+  const [permission, requestPermission] = useCameraPermissions()
+
+  const [appState, setAppState] = useState(AppState.currentState)
+  const [canScan, setCanScan] = useState(true)
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', nextAppState => {
+      console.log('nextAppState', nextAppState)
+      setAppState(nextAppState)
+      setCanScan(nextAppState === 'active')
+    })
+
+    return () => {
+      subscription.remove()
+    }
+  }, [])
+
+  if (!permission) {
+    return (
+      <View>
+        <Text style={styles.text}>没权限</Text>
+      </View>
+    )
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    )
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'))
+  }
+
+  const openURL = url => {
+    Linking.openURL(url).catch(err => console.error('An error occurred', err))
+  }
+
+  return (
+    <View style={styles.container}>
+      <CameraView
+        style={styles.camera}
+        facing={facing}
+        onBarcodeScanned={barcode => {
+          if (!canScan) {
+            return
+          }
+          const url = barcode.data
+          if (isValidURL(url)) {
+            setCanScan(false)
+            openURL(url)
+          } else {
+            console.log('Invalid URL:', url)
+          }
+        }}
+      >
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => setCanScan(true)}>
+            <Text style={styles.text}>扫码</Text>
+            <Text style={styles.text}>appState: {appState}</Text>
+            <Text style={styles.text}>canScan: {String(canScan)}</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+    </View>
+  )
+}
+
+function isValidURL(text) {
+  const urlPattern = /^(https?:\/\/|www\.)[^\s/$.?#].[^\s]*$/i
+  return urlPattern.test(text)
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10
+  },
+  camera: {
+    flex: 1
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white'
+  }
+})
